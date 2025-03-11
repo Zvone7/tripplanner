a256ce612984dbd208d7b2a3c3c6689b78dad821
zvonimir <grubisic.zvonimir@gmail.com>
2025-03-11 22:02:08 +0100

    change way keyvault is loaded

--- backend/TripPlanner/Web/Program.cs
+++ backend/TripPlanner/Web/Program.cs
@@ -38,10 +38,15 @@ public class Program
         builder.Configuration.AddEnvironmentVariables();
         builder.Configuration.AddJsonFile("appsettings.json", optional: false);
         Console.WriteLine($"{DateTime.UtcNow}|appsettings loaded");
-        LoadKeyVault(builder);
-#if DEBUG
+        var keyVaultName = builder.Configuration["KEYVAULT_NAME"];
+#if RELEASE
+        LoadKeyVaultViaDefaultCredential(builder, keyVaultName);
+#else
         builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true);
         Console.WriteLine($"{DateTime.UtcNow}|appsettings.dev loaded");
+        LoadKeyVaultViaAppRegCredential(builder, keyVaultName);
+        builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true);
+        Console.WriteLine($"{DateTime.UtcNow}|appsettings.dev re-loaded to overwrite keyvault configs");
 #endif
         var appSettings = InitializeAppSettings(builder);
 
@@ -70,20 +75,31 @@ public class Program
         return appSettings;
     }
 
-    private static void LoadKeyVault(WebApplicationBuilder builder)
+    private static void LoadKeyVaultViaDefaultCredential(WebApplicationBuilder builder, string keyVaultName)
     {
-        var keyVaultName = builder.Configuration["KEYVAULT_NAME"];
+        Console.WriteLine($"{DateTime.UtcNow}|Using keyvault {keyVaultName} (default azure credential).");
         var keyvaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
-#if DEBUG
-        var tenantId = builder.Configuration["TENANT_ID"];
-        var clientId = builder.Configuration["CLIENT_ID"];
-        var clientSecret = builder.Configuration["CLIENT_SECRET"];
-        var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-#else
         var clientSecretCredential = new DefaultAzureCredential();
-#endif
+        builder.Configuration.AddAzureKeyVault(keyvaultUri, clientSecretCredential);
+        Console.WriteLine($"{DateTime.UtcNow}|Keyvault loaded");
+    }
 
-        Console.WriteLine($"{DateTime.UtcNow}|Using keyvault {keyVaultName}");
+
+    private static void LoadKeyVaultViaAppRegCredential(WebApplicationBuilder builder, string keyVaultName)
+    {
+        Console.WriteLine($"{DateTime.UtcNow}|Using keyvault {keyVaultName} (clientid).");
+        var keyvaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+        var clientId = builder.Configuration.GetValue<string>("AzureAd:ClientId");
+        if (string.IsNullOrWhiteSpace(clientId))
+            clientId = builder.Configuration["CLIENT_ID"];
+        var tenantId = builder.Configuration.GetValue<string>("AzureAd:TenantId");
+        if (string.IsNullOrWhiteSpace(tenantId))
+            tenantId = builder.Configuration["TENANT_ID"];
+        var clientSecret = builder.Configuration.GetValue<string>("AzureAd:ClientSecret");
+        if (string.IsNullOrWhiteSpace(clientSecret))
+            clientSecret = builder.Configuration["CLIENT_SECRET"];
+        var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
         builder.Configuration.AddAzureKeyVault(keyvaultUri, clientSecretCredential);
         Console.WriteLine($"{DateTime.UtcNow}|Keyvault loaded");
     }
